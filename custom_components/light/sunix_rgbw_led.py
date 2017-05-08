2ba64726429b4bb1ebc25da5fc662ebd9c252148
Markus Ressel <markus9100@gmail.com>
2017-05-08 03:59:25 +0200

    fix for removal of ordered_dict validator

--- custom_components/light/sunix_rgbw_led.py
+++ custom_components/light/sunix_rgbw_led.py
@@ -5,7 +5,6 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/light.sunix/
 """
 
-import colorsys
 import logging
 from enum import Enum
 
@@ -17,10 +16,8 @@ from homeassistant.components.light import (
 from homeassistant.const import CONF_PLATFORM, CONF_NAME, DEVICE_DEFAULT_NAME, CONF_HOST
 from homeassistant.util.color import (
     color_temperature_mired_to_kelvin as mired_to_kelvin,
-    color_temperature_kelvin_to_mired as kelvin_to_mired,
     color_temperature_to_rgb,
     color_rgb_to_rgbw,
-    color_rgbw_to_rgb,
     color_RGB_to_xy)
 from sunix_ledstrip_controller_client import LEDStripControllerClient
 from sunix_ledstrip_controller_client.controller import Controller
@@ -45,7 +42,7 @@ PLATFORM_SCHEMA = vol.Schema({
         vol.All(vol.Coerce(int), vol.Range(min=1, max=65535)),
     vol.Optional(CONF_NAME, default=DEVICE_DEFAULT_NAME): cv.string,
     vol.Optional(CONF_CALIBRATION_OFFSET, default=None):
-        cv.ordered_dict(vol.All(vol.Coerce(int), vol.Range(min=-255, max=255)), cv.string),
+        vol.All({cv.string: vol.All(vol.Coerce(int), vol.Range(min=-255, max=255))}),
     vol.Optional(CONF_EFFECT_SPEED, default=DEFAULT_EFFECT_SPEED):
         vol.All(vol.Coerce(int), vol.Range(min=1, max=255))
 }, extra=vol.ALLOW_EXTRA)
@@ -86,23 +83,24 @@ class SunixController(Light):
         self._api = api
         self._device = device
 
-        self._rgb = [255, 255, 255]  # initial color
-        self._brightness = 255  # initial brightness
+        self.update()
+
+        self._rgb = self._device.get_rgbww()  # initial color
+        self._brightness = self._device.get_brightness()  # initial brightness
         self._color_temp = 154  # initial color temp (most blueish)
         self._effect = None
+        # self._effect = self._device.get_effect()
         self._effect_speed = effect_speed
         self._color_mode = ColorMode.RGB  # rgb color
         self._color_offset = color_offset  # calibration offset
 
-        self._api.update_state(self._device)
-
     def get_rgbww_with_brightness(self, rgb) -> [int, int, int, int, int]:
-        _LOGGER.error("rgb: %s", rgb)
+        _LOGGER.debug("rgb: %s", rgb)
 
         if self._color_mode == ColorMode.COLOR_TEMPERATURE:  # color temperature
-            _LOGGER.error("temp: %s", self._color_temp)
+            _LOGGER.debug("temp: %s", self._color_temp)
             rgb = color_temperature_to_rgb(mired_to_kelvin(self._color_temp))
-            _LOGGER.error("rgb color_temp: %s", rgb)
+            _LOGGER.debug("rgb color_temp: %s", rgb)
 
             # apply color offset if any
             if self._color_offset:
@@ -111,7 +109,7 @@ class SunixController(Light):
                     min(rgb[1] + self._color_offset[1], 255),
                     min(rgb[2] + self._color_offset[2], 255)
                 ]
-                _LOGGER.error("rgb after offset: %s", rgb)
+                _LOGGER.debug("rgb after offset: %s", rgb)
 
         rgbw = list(color_rgb_to_rgbw(rgb[0], rgb[1], rgb[2]))
         rgbw.append(rgbw[3])  # duplicate warm_white value for cold_white
@@ -124,9 +122,51 @@ class SunixController(Light):
         for color in rgbw:
             calculated_color.append(int(color * (self._brightness / 255)))
 
-        _LOGGER.error("RGBWW after Brightness: %s", calculated_color)
+        _LOGGER.debug("RGBWW after Brightness: %s", calculated_color)
 
         return calculated_color
+
+    def check_args(self, turn_on, **kwargs):
+        rgb = kwargs.get(ATTR_RGB_COLOR)
+        brightness = kwargs.get(ATTR_BRIGHTNESS)
+        color_temp = kwargs.get(ATTR_COLOR_TEMP)
+        effect = kwargs.get(ATTR_EFFECT)
+
+        if rgb:
+            _LOGGER.debug("rgb: %s", rgb)
+            self._rgb = rgb
+            self._color_mode = ColorMode.RGB
+        elif color_temp:
+            _LOGGER.debug("color_temp: %s", color_temp)
+            self._color_temp = color_temp
+            self._color_mode = ColorMode.COLOR_TEMPERATURE
+        elif effect:
+            _LOGGER.debug("effect: %s", effect)
+            self._effect = effect
+            self._color_mode = ColorMode.EFFECT
+
+        _LOGGER.debug("color_mode: %s", self._color_mode)
+
+        if brightness:
+            _LOGGER.debug("brightness: %s", brightness)
+            self._brightness = brightness
+
+        if self._color_mode != ColorMode.EFFECT:
+            c = self.get_rgbww_with_brightness(self._rgb)
+            _LOGGER.debug("setting color %s on %s", c, self._name)
+            self._api.set_rgbww(self._device, c[0], c[1], c[2], c[3], c[4])
+        else:
+            _LOGGER.debug("setting function %s on %s", self._effect, self._name)
+            self._api.set_function(self._device, FunctionId[self._effect], self._effect_speed)
+
+        if turn_on:
+            if not self.is_on:
+                _LOGGER.debug("turning on %s", self._name)
+                self._api.turn_on(self._device)
+        else:
+            if self.is_on:
+                _LOGGER.debug("turning off %s", self._name)
+                self._api.turn_off(self._device)
 
     @property
     def unique_id(self):
@@ -185,42 +225,11 @@ class SunixController(Light):
 
     def turn_on(self, **kwargs):
         """Turn the light on"""
-        rgb = kwargs.get(ATTR_RGB_COLOR)
-        brightness = kwargs.get(ATTR_BRIGHTNESS)
-        color_temp = kwargs.get(ATTR_COLOR_TEMP)
-        effect = kwargs.get(ATTR_EFFECT)
-
-        if rgb:
-            _LOGGER.debug("rgb: %s", rgb)
-            self._rgb = rgb
-            self._color_mode = ColorMode.RGB
-        elif color_temp:
-            _LOGGER.debug("color_temp: %s", color_temp)
-            self._color_temp = color_temp
-            self._color_mode = ColorMode.COLOR_TEMPERATURE
-        elif effect:
-            _LOGGER.debug("effect: %s", effect)
-            self._effect = effect
-            self._color_mode = ColorMode.EFFECT
-
-        _LOGGER.debug("color_mode: %s", self._color_mode)
-
-        if brightness:
-            _LOGGER.debug("brightness: %s", brightness)
-            self._brightness = brightness
-
-        if self._color_mode != ColorMode.EFFECT:
-            c = self.get_rgbww_with_brightness(self._rgb)
-            self._api.set_rgbww(self._device, c[0], c[1], c[2], c[3], c[4])
-        else:
-            self._api.set_function(self._device, FunctionId[self._effect], self._effect_speed)
-
-        if not self._device.is_on():
-            self._api.turn_on(self._device)
+        self.check_args(True, **kwargs)
 
     def turn_off(self, **kwargs):
         """Turn the light off"""
-        self._api.turn_off(self._device)
+        self.check_args(False, **kwargs)
 
     def update(self):
         """Update the state of this light."""
